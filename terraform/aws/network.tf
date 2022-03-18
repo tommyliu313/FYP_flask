6673f09015aae3df777a600537a5becb74c0487c
tommyliu313 <liuchaktin@gmail.com>
2022-03-18 21:51:02 +0800

    20220318 2150 update

--- terraform/aws/network.tf
+++ terraform/aws/network.tf
@@ -2,6 +2,25 @@ module "vpc"{
 
 }
 
-resource "aws_security_group" "alb_sg"{
+resource "aws_security_group" "nginx-sg" {
+  name   = "${local.name_prefix}-nginx_sg"
+  vpc_id = aws_vpc.vpc.id
 
+  # HTTP access from VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
 }
