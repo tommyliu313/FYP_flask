48049958b247673fca67291340f37ffbcb73485c
Tommyliu313 <[liuchaktin@gmail.com]>
2022-04-19 02:07:39 +0800

    20220419 0207 update

--- other/terraform/azure/variable.tf
+++ other/terraform/azure/variable.tf
@@ -12,6 +12,10 @@ variable "location"{
  type = string
  default = "eastasia"
 }
+variable "resource_group_name" {
+  type = string
+  default = ""
+}
 
 #virtual networking
 variable "vnet_cidr_range"{
@@ -19,16 +23,13 @@ variable "vnet_cidr_range"{
 }
 variable "vnet_id"{}
 variable "vnet_name"{}
+#subnet
 variable "public_subnet"{
 type = string
 default = ["10.0.0.0/24"]
 }
 variable "private_subnet"{
  type = string
-}
-variable "resource_group_name" {
-  type = string
-  default = ""
 }
 variable "subnet_prefixes"{
   type = list(string)
@@ -37,6 +38,7 @@ variable "subnet_names" {
  type = string
 }
 
+#kubernetes
 variable "kubernetes_settings"{
   name = "example-aks1"
   location = var.location
