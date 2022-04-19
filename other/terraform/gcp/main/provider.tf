6fb71ed91f8c7ecb82be5e06108450cf75d70e03
TKM0325 <52584190+TKM0325@users.noreply.github.com>
2022-04-20 02:18:08 +0800

    Update provider.tf

--- other/terraform/gcp/main/provider.tf
+++ other/terraform/gcp/main/provider.tf
@@ -8,10 +8,11 @@ provider "google" {
 
 resource "google_compute_instance" "default" {
   name         = "test"
-  machine_type = "e2-medium"
-  zone         = "asis-east2-a"
+  machine_type = "e2-micro"
+  region      = "asia-east2"
+  zone        = "asia-east2-a"
 
-  tags = ["v", "m"]
+  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
@@ -33,40 +34,93 @@ resource "google_compute_instance" "default" {
   }
 
   metadata = {
-    v = "m"
+    foo = "bar"
   }
 
   metadata_startup_script = "echo hi > /test.txt"
 
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = kiko2
+    email  = "kiko00012016@gmail.com"
     scopes = ["cloud-platform"]
   }
 }
 
+resource "google_compute_instance" "default" {
+  name         = "test2"
+  machine_type = "e2-micro"
+  region      = "asia-east2"
+  zone        = "asia-east2-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "kiko00012016@gmail.com"
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_disk" "default" {
+  name  = "disk1"
+  type  = "pd-ssd"
+  zone  = "asia-east2-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
+resource "google_compute_disk" "default" {
+  name  = "disk2"
+  type  = "pd-ssd"
+  zone  = "asia-east2-a"
+  image = "debian-9-stretch-v20200805"
+  labels = {
+    environment = "dev"
+  }
+  physical_block_size_bytes = 4096
+}
+
 resource "google_sql_database" "database" {
-  name     = "projectmysqltest"
+  name     = "my-database"
   instance = google_sql_database_instance.instance.name
 }
 
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "projectmysqltest" {
-  name             = "projectmysqltest"
-  region           = "asia-east2"
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = "us-central1"
   database_version = "MYSQL_5_7"
   settings {
     tier = "db-f1-micro"
   }
 
   deletion_protection  = "true"
-}
-
-provider "kubernetes"{
-  version = "~> 1.10.0"
-  host = google_container_cluster.default.endpoint
-  token = data.google_client_config.current.access_token
-  client_certificate = base64encode(google_container_cluster.default.master_auth[0].client_certificate,)
-  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
-  cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate,)
 }
