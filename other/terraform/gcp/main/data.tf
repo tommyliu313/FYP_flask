27d7bcd1009bc3388cc6ebd3f6c9fc13adae02ce
TKM0325 <52584190+TKM0325@users.noreply.github.com>
2022-04-20 04:15:10 +0800

    Update data.tf

--- other/terraform/gcp/main/data.tf
+++ other/terraform/gcp/main/data.tf
@@ -1,3 +1,14 @@
-data "google_client_config" "default"{
-  location = var.location
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "cluster"
+  location = "asia-east2"
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
 }
