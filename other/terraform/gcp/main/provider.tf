36d78e00bb43ac95ee5646998488fc07b19643f1
TKM0325 <52584190+TKM0325@users.noreply.github.com>
2022-04-20 00:48:21 +0800

    Update provider.tf

--- other/terraform/gcp/main/provider.tf
+++ other/terraform/gcp/main/provider.tf
@@ -1,7 +1,48 @@
 provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.location
+    credentials = file("itp4121project-da9e77c5e6ab.json")
+    project     = "itp4121project"
+    region      = "asia-east2"
+    zone        = "asia-east2-a"
+
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "asis-east2-a"
+
+  tags = ["v", "m"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    v = "m"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = kiko2
+    scopes = ["cloud-platform"]
+  }
 }
 
 provider "kubernetes"{
